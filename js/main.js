a319371c0546eb4419d02dd3f99106ba476831a0
diyanya <butorinadn@gmail.com>
2025-06-13 00:25:30 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu__link');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu--open')
+    menu.classList.toggle('menu--open');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+            menu.classList.remove('menu--open');
+        }
+    });
 });
